2645aae0c802072b9a9ebde0ec8dc259347180cd
atillatahak <atillatahaa@gmail.com>
2023-02-12 20:51:48 +0300

    added func which name loadtemplates

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
 
 type Rsvp struct{
 	Name, Email, Phone string
@@ -8,7 +11,20 @@ type Rsvp struct{
 }
 
 var responses = make([]*Rsvp,0,10)
+var templates = make(map[string]*template.Template,3)
 func main(){
-	fmt.Println("TODO: add some features")
+	loadTemplates()
 }
 
+func loadTemplates(){
+	templateNames :=[5]string {"welcome","form","thanks","sorry","list"}
+	for index, name := range templateNames{
+		t, err := template.ParseFiles("layout.html",name+".html")
+		if(err == nil){
+			templates[name] = t
+			fmt.Println("Loaded template", index,name)
+		}else{
+			panic(err)
+		}
+	}
+}
