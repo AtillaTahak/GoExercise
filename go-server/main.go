cda0e2528c0603d080868fe34a84e00554dde29e
atillatahak <atillatahaa@gmail.com>
2023-02-19 22:29:16 +0300

    Added new exercise

---
+++ go-server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/",fileServer)
+	http.HandleFunc("/form",formHandler)
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Printf("Starting Server At port 8080\n")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+func formHandler(writer http.ResponseWriter, request *http.Request){
+	if err:= request.ParseForm(); err !=nil{
+		fmt.Fprintf(writer, "ParseForm err: %v",err)
+		return
+	}
+	fmt.Fprintf(writer,"Post request !")
+	name := request.FormValue("name")
+	address := request.FormValue("address")
+	fmt.Fprintf(writer, "Name = %s\n",name)
+	fmt.Fprintf(writer, "Address = %s\n",address)
+
+}
+
+func helloHandler(writer http.ResponseWriter, request *http.Request){
+	if request.URL.Path != "hello"{
+		http.Error(writer,"404 not found",http.StatusNotFound)
+		return
+	}
+	if request.Method != "GET"{
+		http.Error(writer,"404 not found",http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(writer,"Hello!")
+}
